8817f573dbdae3bfde9ebf2148f598bd884fcc4e
lol5115 <lol5115@208-WD05.up.ist.local>
2014-12-09 19:24:51 -0500

    Fixed enemy hit logic, and passed an instance of infoPanel into UserPanel.

--- src/battleship/MainPanel.java
+++ src/battleship/MainPanel.java
@@ -1,6 +1,5 @@
 package battleship;
 
-import static battleship.UserPanel.layout;
 import java.awt.GridLayout;
 import javax.swing.*;
 import java.util.ArrayList;
@@ -22,8 +21,8 @@ public class MainPanel extends JPanel {
         layout = new GridLayout(1, 3);
         setLayout(layout);
 
-        userPanel = new UserPanel();
         infoPanel = new InfoPanel();
+        userPanel = new UserPanel(infoPanel);
         enemyPanel = new EnemyPanel(infoPanel);
         add(userPanel);
         add(infoPanel);
@@ -32,30 +31,25 @@ public class MainPanel extends JPanel {
         enemy = new ArrayList();
         enemyPanel.setEnemyShips();
 
-        
     }
-    
-    public void startGame()
-    {
-        
-        
+
+    public void startGame() {
+
         /*
-        do
-            move
-        while nobody has won yet
-        */
+         do
+         move
+         while nobody has won yet
+         */
         move();
     }
-    
+
     /*
-    This method executes one round of moves - one move from
-    the user and one move from the enemy.
-    */
-    public void move()
-    {
+     This method executes one round of moves - one move from
+     the user and one move from the enemy.
+     */
+    public void move() {
         int spaceToAttack = enemyPanel.selectTarget();
         userPanel.handleEnemyAttack(spaceToAttack);
     }
-    
 
 }
